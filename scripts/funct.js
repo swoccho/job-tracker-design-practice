d28d6b3b0340f72e12dc3a64355d035561b63e1e
swoccho <asikurrs09@gmail.com>
2026-07-15 00:31:13 +0600

    issue fixed for the all tab , where now it will show all the jobs with status updated in all tab and in their specific status tab

--- scripts/funct.js
+++ scripts/funct.js
@@ -20,24 +20,16 @@ function switchTab(tab)
     for(const t of tabs)
     {
         const tabName = document.getElementById("tab-"+t);
-        const selected = document.getElementById(t+"-container");
+        
         console.log(tabName);
 
         if(t === tab)
         {
             tabName.classList.add(...tabActive);
-            
-            selected.classList.remove("hidden");
-
-            if(selected.children.length < 1)
-            {
-                emptystate.classList.remove("hidden");
-            }
 
         }
         else{
             tabName.classList.remove(...tabActive);
-            selected.classList.add("hidden");
 
         }    
     }  
@@ -57,7 +49,7 @@ document.getElementById("jobs-card-container").addEventListener("click", functio
     const card = clickedElement.closest(".card");
 
     const status = card.querySelector(".js-status");
-    const parent = card.parentNode;
+    // const parent = card.parentNode;
 
     
     if(clickedElement.classList.contains("interview"))
@@ -65,9 +57,12 @@ document.getElementById("jobs-card-container").addEventListener("click", functio
         console.log("Interview clicked");
         status.innerText = "Interviewed";
 
-        const clonedcard = card.cloneNode(true);
+        status.classList.remove("bg-slate-300" , "border-red-600" , "text-red-600");
+        status.classList.add("border-green-600" , "text-green-600");
 
-        interviewContainer.appendChild(clonedcard);
+        // const clonedcard = card.cloneNode(true);
+
+        // interviewContainer.appendChild(clonedcard);
         updateCount();
 
     }
@@ -76,15 +71,22 @@ document.getElementById("jobs-card-container").addEventListener("click", functio
     {
         console.log("rejected clicked");
         status.innerText = "Rejected";
-        const clonedcard = card.cloneNode(true);
-        rejectedContainer.appendChild(clonedcard);
+
+        status.classList.remove("bg-slate-300" , "border-green-600" , "text-green-600");
+        status.classList.add("border-red-600" , "text-red-600");
+
+
+
+
+        // const clonedcard = card.cloneNode(true);
+        // rejectedContainer.appendChild(clonedcard);
         updateCount();
     }
 
     if(clickedElement.classList.contains("delete"))
     {
         console.log("delete clicked");
-        parent.removeChild(card);
+        card.remove();
         updateCount();
 
 
@@ -103,7 +105,7 @@ const rejectedCount = document.getElementById("rejected-count");
 // rejectedCount.innerHTML = rejectedContainer.children.length;
 
 
-updateCount();
+
 
 function updateCount()
 {
@@ -114,20 +116,63 @@ function updateCount()
 
 
     const counts = {
-        all : allcontainer.children.length,
-        interview : interviewContainer.children.length,
-        rejected : rejectedContainer.children.length,
+        all : 0,
+        interview : 0,
+        rejected : 0,
 
     };
+
+    const cards = allcontainer.querySelectorAll(".card");
+
+    let showCount = 0;
+
+    cards.forEach(card => {
+
+        const statusText = card.querySelector(".js-status").innerText;
+
+        counts.all++;
+
+        if(statusText ==="Interviewed"){
+            counts.interview++;
+        }
+        if(statusText==="Rejected"){
+            counts.rejected++;
+        }
+
+        if(currentTab ==="all")
+        {
+            card.classList.remove("hidden");
+            showCount++;
+        }
+
+        else if(currentTab === "interview" && statusText==="Interviewed")
+        {
+            card.classList.remove("hidden");
+            showCount++;
+        }
+
+        else if(currentTab ==="rejected" && statusText==="Rejected")
+            {
+                card.classList.remove("hidden");
+                showCount++;
+            } 
+        
+        else{
+            card.classList.add("hidden");
+        }
+
+    })
+
+
 
     allCount.innerText = counts.all;
     interviewCount.innerText = counts.interview;
     rejectedCount.innerText = counts.rejected;
 
-    available.innerText = counts[currentTab];
+    available.innerText = showCount;
 
 
-       if(counts[currentTab] < 1)
+    if(showCount < 1)
     {
         emptystate.classList.remove("hidden");
 
@@ -135,10 +180,6 @@ function updateCount()
     else{
         emptystate.classList.add("hidden");
     }
-
-        
-    
-
 
 }
 
